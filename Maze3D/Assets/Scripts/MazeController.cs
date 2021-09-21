8d1d12fb737ac99344cfd8ddde7526cdd40e2ba7
vanlinh00 <82046592+vanlinh00@users.noreply.github.com>
2021-09-21 16:05:27 +0700

    create project

---
+++ Maze3D/Assets/Scripts/MazeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//https://www.youtube.com/watch?v=u8UbHJCDJjY
+//https://weblog.jamisbuck.org/2011/1/24/maze-generation-hunt-and-kill-algorithm
+
+public class MazeController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
