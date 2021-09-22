a17007108d6632fbda75b9d68f642e7029040c77
vanlinh00 <82046592+vanlinh00@users.noreply.github.com>
2021-09-22 15:40:50 +0700

    create project

--- Maze3D/Assets/Scripts/MazeController.cs
+++ Maze3D/Assets/Scripts/MazeController.cs
@@ -6,10 +6,31 @@ using UnityEngine;
 
 public class MazeController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int height;
+    public int weight;
+    public GameObject Wall;
+    public GameObject Plane;
     void Start()
     {
-        
+        Vector3 change = new Vector3();
+        change.x = transform.position.x;
+        change.y = transform.position.y;
+        change.z = transform.position.z;
+        for (int i=0;i<height;i++ )
+        {
+            change.x = i * 3.5f;
+            change.z = 3.5f;
+            transform.position = change;
+            for (int j=0;j<weight ;j++ )
+            {
+                change.z = transform.position.z+3.5f;
+                transform.position = change;
+                Mazascell cell = new Mazascell();
+                GameObject Obcell = Instantiate(Plane, transform.position, Quaternion.identity);
+
+            }
+            
+        }    
     }
 
     // Update is called once per frame
--- Maze3D/Assets/Scripts/Mazecell.cs
+++ Maze3D/Assets/Scripts/Mazecell.cs
@@ -2,17 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Mazascell : MonoBehaviour
+public class Mazascell 
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public bool Isvisited = false;
+    public GameObject DownWall;
+    public GameObject UpWall;
+    public GameObject LeftWall;
+    public GameObject RigthWall;
 }
